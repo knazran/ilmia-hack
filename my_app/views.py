83fa922646cf328ad7e884b6de1c6b8a0a5e382f
Knazran <khairulnazran94@gmail.com>
2020-07-11 14:27:46 +0800

    Industry chart

--- my_app/views.py
+++ my_app/views.py
@@ -23,7 +23,18 @@ def survey(request):
     template = 'survey.html'
     return render(request, template, context)
 
+# data = {
+#     datasets: [{
+#         data: [10, 20, 30]
+#     }],
 
+#     // These labels appear in the legend and in the tooltips when hovering different arcs
+#     labels: [
+#         'Red',
+#         'Yellow',
+#         'Blue'
+#     ]
+# };
 def results(request):
     # json_data = json.loads(request.body)
     form = SurveyFormStepOne(request.POST)  # A form bound to the POST data
@@ -31,13 +42,25 @@ def results(request):
         print(form.cleaned_data['course_of_study'])
         course = form.cleaned_data['course_of_study']
 
-        salary_data = df_tracer.query("nec1_desc=='{}' & age < 25".format(course))['monthly_income']\
-            .value_counts(1).to_json()
-        print(salary_data)
+        df_fresh_grad = df_tracer.query("nec1_desc=='{}' & age < 25".format(course))
+
+        salary_data = df_fresh_grad['monthly_income']\
+            .value_counts(1).round(3)
+        salary_chart_payload = {
+            'datasets': [{
+                'data': list(salary_data.values * 100)
+            }],
+            'labels': list(salary_data.keys())
+        }
+        top_5_industries_labels = list(df_fresh_grad['msic1'].value_counts(1)[:5].keys())
+        top_5_industries_values = list(df_fresh_grad['msic1'].value_counts(1).round(3)[:5].values * 100)
+        
     context = {
         "course": course,
         "average_salary": 3000,
-        "salary_data": salary_data
+        "salary_chart_payload": salary_chart_payload,
+        "top_5_industries_labels": top_5_industries_labels,
+        "top_5_industries_values": top_5_industries_values,
         # "university": json_data["university"],
         # "location": json_data["location"],
     }
